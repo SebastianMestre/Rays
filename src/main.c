080d73bb4320b0364b40f031323f54e3a8aa80e4
SebastianMestre <mestresebastianunr@gmail.com>
2020-02-23 11:15:08 -0300

    Use a bucket array to store and process samples more quickly

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 // main.c
 #include <math.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "bmp.h"
@@ -8,20 +9,28 @@
 #include "vectors.h"
 #include "intersection.h"
 
-#define W 128
-#define H 128
+#define W 512
+#define H 512
+
+#define BW 2
+#define BH 2
+
+#define BCOLS (W/BW)
+#define BROWS (H/BH)
+
 #define SPP 10
+#define SPB (BW*BH*SPP)
 #define SC (W*H*SPP)
 
 typedef struct {
-	float r,g,b;
+	V3 color;
 	float x,y;
 } Sample;
 
 uint8_t r[W][H];
 uint8_t g[W][H];
 uint8_t b[W][H];
-Sample samples[SC];
+Sample samples[BCOLS][BROWS][BW*BH*SPP];
 
 #define PLANE_COUNT 3
 Plane planes[PLANE_COUNT] = {
@@ -47,6 +56,7 @@ V3 sample_hemisphere_cosine_weighted (float r1, float r2) {
 	};
 }
 
+
 Intersection trace (Ray r) {
 	Intersection result = no_intersection();
 
@@ -71,40 +81,26 @@ Intersection trace (Ray r) {
 	return result;
 }
 
-int main () {
+V3 full_trace (Ray r) {
 
-	V3 camera_pos = {0.0f, 0.0f, 0.0f};
-	V3 camera_up = {0.0f, 0.0f, 1.0f};
-	V3 camera_right = {};
-	V3 camera_front = {};
+	const V3 sky_color = V3_scale((V3){0.1f, 0.6f, 1.0f}, 10);
 
-	int s = 0;
-	for (int s = 0; s < SC; ++s){
-		float px = (float)rand() / (RAND_MAX);
-		float py = (float)rand() / (RAND_MAX);
+	Intersection result = trace(r);
 
-		printf("(%.2f %.2f)\n", px, py);
+	if (!result.exists) {
+		return sky_color;
+	}
 
-		V3 screen = {
-			px*2.0f-1.0f,
-			py*2.0f-1.0f,
-			1.0f
-		};
+	// TODO: Not so sure about this stuff
+	const float i_pi = 0.31830988618;
+	float factor = 1.0f;
 
-		V3 view = V3_normalized(screen);
+	// for up to three bounces, we try to find a light source
+	for(int steps = 0; steps < 3; ++steps){
 
-		Intersection result = trace((Ray){camera_pos, view});
+		factor *= i_pi;
 
-		if (!result.exists) {
-			samples[s] = (Sample){
-				1.0f, 1.0f, 1.0f,
-				px, py
-			};
-			continue;
-		}
-
-		// Ambient Occlusion
-		V3 up = {0.0f, 0.0f, 1.0f};
+		V3 up = (V3){0.0f, 0.0f, 1.0f};
 		V3 side = V3_normalized(V3_cross(up, result.normal));
 		up = V3_normalized(V3_cross(side, result.normal));
 
@@ -123,68 +119,106 @@ int main () {
 			bounce_direction,
 		};
 
-		Intersection bounce_result = trace(bounce_ray);
+		result = trace(bounce_ray);
 
-		if (bounce_result.exists){
-			// hit something, no light
-			samples[s] = (Sample){
-				0.0f, 0.0f, 0.0f,
-				px, py
-			};
-		} else {
-			// hit nothing, skylight
-			samples[s] = (Sample){
-				1.0f, 1.0f, 1.0f,
-				px, py
-			};
+		// no hits, found the sky
+		if (!result.exists){
+			return V3_scale(sky_color, i_pi);
 		}
 	}
 
+	// didn't find a light source
+	return (V3){0.0f, 0.0f, 0.0f};
+}
 
-	for(int x = 0; x < W; ++x){
-		for(int y = 0; y < H; ++y){
-			float px = (float)x/(W-1);
+int main () {
+
+	V3 camera_pos = {0.0f, 0.0f, 0.0f};
+	V3 camera_up = {0.0f, 0.0f, 1.0f};
+	V3 camera_right = {};
+	V3 camera_front = {};
+
+	printf("# SPB = %d\n", SPB);
+	for (int bx = 0; bx < BCOLS; ++bx){
+		for (int by = 0; by < BROWS; ++by){
+			for (int sb = 0; sb < SPB; ++sb) {
+				float px_in_bucket = (float)rand() / (float)RAND_MAX;
+				float py_in_bucket = (float)rand() /  (float)RAND_MAX;
+
+				float px = ((float)bx + px_in_bucket) / (float)BCOLS;
+				float py = ((float)by + py_in_bucket) /  (float)BROWS;
+
+				V3 screen = {
+					px*2.0f-1.0f,
+					py*2.0f-1.0f,
+					1.0f
+				};
+
+				V3 view_direction = V3_normalized(screen);
+
+				Ray view = {camera_pos, view_direction};
+
+				V3 color = full_trace(view);
+				samples[bx][by][sb] = (Sample){color, px, py};
+			}
+		}
+	}
+
+	for (int x = 0; x < W; ++x) {
+		float px = (float)x/(W-1);
+
+		// printf("[%d] => %d\n", x, bx);
+
+		for (int y = 0; y < H; ++y) {
 			float py = (float)y/(H-1);
 
-			double pr = 0.0;
-			double pg = 0.0;
-			double pb = 0.0;
+			V3 pcolor = {0.0f, 0.0f, 0.0f};
 			double coeffs = 0.0;
 
-			for(int s = 0; s < SC; ++s){
-				float sx = samples[s].x;
-				float sy = samples[s].y;
+			int bcx = x / BW;
+			int bcy = y / BH;
 
-				float dx = (sx - px)*W*2;
-				float dy = (sy - py)*H*2;
+			int bx_start = bcx-1 < 0 ? 0 : bcx-1;
+			int by_start = bcy-1 < 0 ? 0 : bcy-1;
 
-				float d2 = dx*dx + dy*dy;
+			for (int bx = bx_start; bx<=bcx+1 && bx<BCOLS; ++bx) {
+				for (int by = by_start; by<=bcy+1 && by<BROWS; ++by) {
+					for (int sb = 0; sb < SPB; ++sb) {
+						float sx = samples[bx][by][sb].x;
+						float sy = samples[bx][by][sb].y;
 
-				float coeff = exp(-d2);
+						float dx = (sx - px)*W*2;
+						float dy = (sy - py)*H*2;
 
-				pr += samples[s].r * coeff;
-				pg += samples[s].g * coeff;
-				pb += samples[s].b * coeff;
-				coeffs += coeff;
+						float d2 = dx*dx + dy*dy;
+
+						float coeff = exp(-d2);
+
+						pcolor = V3_sum(pcolor, V3_scale(samples[bx][by][sb].color, coeff));
+						coeffs += coeff;
+					}
+				}
 			}
 
-			pr /= coeffs;
-			pg /= coeffs;
-			pb /= coeffs;
+			pcolor = V3_scale(pcolor, 1/coeffs);
 
-			r[x][y] = pr * 255;
-			g[x][y] = pg * 255;
-			b[x][y] = pb * 255;
+			pcolor.x = 2 * pcolor.x / (2 * pcolor.x + 1);
+			pcolor.y = 2 * pcolor.y / (2 * pcolor.y + 1);
+			pcolor.z = 2 * pcolor.z / (2 * pcolor.z + 1);
+
+			r[x][y] = pcolor.x * 255;
+			g[x][y] = pcolor.y * 255;
+			b[x][y] = pcolor.z * 255;
 		}
 	}
 
-	for(int x = 0; x < W; ++x){
-		for(int y = 0; y < H; ++y){
+	for (int x = 0; x < W; ++x) {
+		for (int y = 0; y < H; ++y) {
 			r[x][y] = 255 - r[x][y];
 			g[x][y] = 255 - g[x][y];
 			b[x][y] = 255 - b[x][y];
 		}
 	}
 
-	bmp_write("helper.bmp", (uint8_t*)r,(uint8_t*)g,(uint8_t*)b, W,H);
+	bmp_write("helper.bmp", (uint8_t*)r, (uint8_t*)g, (uint8_t*)b, W, H);
 }
