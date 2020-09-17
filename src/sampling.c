348eb2729ec4c01da99ca9973060afbfa95ee194
SebastianMestre <mestresebastianunr@gmail.com>
2020-09-17 19:42:58 -0300

    implement GGX importance sampling

--- src/sampling.c
+++ src/sampling.c
@@ -7,7 +7,7 @@
 
 V3 sample_hemisphere_cosine_weighted (float r1, float r2) {
 	float r = sqrt(r1);
-	float phi = r2 * M_PI * 2.0f;
+	float phi = r2 * (M_PI * 2.0f);
 
 	return (V3){
 		r * cos(phi),
@@ -35,4 +35,15 @@ V3 sample_dirac_reflection(V3 normal, V3 view) {
 	float ndotv = V3_dot(normal, view);
 
 	V3 result = V3_sub(V3_scale(normal, 2.0f * ndotv), view);
+}
+
+V3 sample_hemisphere_ggx(float r1, float r2, float roughness) {
+	float alpha = roughness * roughness;
+	float theta = acos(sqrt((1.0 - r1) / ((alpha - 1.0) * r1 + 1.0)));
+	float phi = r2 * (M_PI * 2.0f);
+
+	return (V3){
+		sin(theta) * cos(phi),
+		sin(theta) * sin(phi),
+		cos(theta)};
 }
--- src/sampling.h
+++ src/sampling.h
@@ -4,6 +4,7 @@
 #include "vectors.h"
 
 V3 sample_hemisphere_cosine_weighted (float r1, float r2);
+V3 sample_hemisphere_ggx(float r1, float r2, float roughness);
 
 float specular_sample_probablity(V3 normal, V3 view, float r0);
 
